dc95df5f9c2a762af144123fd1bf16009fa70b98
Brian Anderson <andersrb@gmail.com>
2022-07-17 10:51:58 -0600

    start on main

--- components/wasm-opt-sys/src/lib.rs
+++ components/wasm-opt-sys/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod c {
+    use libc::{c_int, c_char};
+
+    #[link(name = "wasm-opt-cc")]
+    extern "C" {
+        pub fn wasm_opt_main(argc: c_int, argv: *const *const c_char) -> c_int;
     }
+}
+
+pub fn wasm_opt_main() -> anyhow::Result<()> {
+    unsafe {
+        c::wasm_opt_main(0, std::ptr::null());
+    }
+    Ok(())
 }
---
+++ components/wasm-opt/src/main.rs
@@ -0,0 +1,3 @@
+fn main() -> anyhow::Result<()> {
+    wasm_opt_sys::wasm_opt_main()
+}
