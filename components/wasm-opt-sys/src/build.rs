93a41f6ac9f1909cce9fe9bc03f483f6a99a7d08
aimeedeer <aimeez@pm.me>
2022-07-21 14:43:57 -0600

    convert wasm_opt.cpp

--- components/wasm-opt-sys/src/build.rs
+++ components/wasm-opt-sys/src/build.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::{Path, PathBuf};
 
 fn main() -> anyhow::Result<()> {
@@ -13,6 +15,7 @@ fn main() -> anyhow::Result<()> {
 
     let tools_dir = src_dir.join("tools");
     let wasm_opt_src = tools_dir.join("wasm-opt.cpp");
+    let wasm_opt_src = get_converted_wasm_opt_cpp(&wasm_opt_src)?;
 
     let flags = ["-Wno-unused-parameter", "-std=c++17"];
 
@@ -23,11 +26,40 @@ fn main() -> anyhow::Result<()> {
     builder
         .cpp(true)
         .include(src_dir)
+        .include(tools_dir)
         .files(src_files)
         .file(wasm_opt_src)
         .compile("wasm-opt-cc");
 
     Ok(())
+}
+
+fn get_converted_wasm_opt_cpp(src_dir: &Path) -> anyhow::Result<PathBuf> {
+    let wasm_opt_file = File::open(src_dir)?;
+    let reader = BufReader::new(wasm_opt_file);
+
+    let output_dir = std::env::var("OUT_DIR")?;
+    let output_dir = Path::new(&output_dir);
+
+    let temp_file_dir = output_dir.join("wasm_opt.cpp.temp");
+    let mut temp_file = File::create(&temp_file_dir)?;
+
+    let mut writer = BufWriter::new(temp_file);
+    for line in reader.lines() {
+        let mut line = line?;
+
+        if line.contains("int main") {
+            line = line.replace("int main", "extern \"C\" int wasm_opt_main");
+        }
+
+        writer.write_all(line.as_bytes())?;
+        writer.write_all(b"\n")?;
+    }
+
+    let output_wasm_opt_file = output_dir.join("wasm-opt.cpp");
+    fs::rename(&temp_file_dir, &output_wasm_opt_file)?;
+
+    Ok(output_wasm_opt_file)
 }
 
 fn get_src_files(src_dir: &Path) -> Vec<PathBuf> {
