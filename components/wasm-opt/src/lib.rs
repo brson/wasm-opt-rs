2a346026ad6c03b589179d4e8d60e153502bfa35
Brian Anderson <andersrb@gmail.com>
2022-07-25 23:14:29 +0000

    Add wasm-opt lib

--- components/wasm-opt/src/lib.rs
+++ components/wasm-opt/src/lib.rs
@@ -1,14 +1,3 @@
-#[cxx::bridge(namespace = "wasm")]
-pub mod ffi {
-    unsafe extern "C++" {
-        include!("shims.h");
-
-        type ModuleReader;
-
-        fn newModuleReader() -> UniquePtr<ModuleReader>;
-    }
-}
-
 /// Hack to establish linage to wasm-opt-sys.
 ///
 /// See docs for wasm_opt_sys::init.
