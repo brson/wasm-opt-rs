f30ff1d5001e07a083002d6ba25d2859526db802
Brian Anderson <andersrb@gmail.com>
2022-07-27 21:36:47 +0000

    more bindings

--- components/wasm-opt-cxx-sys/src/shims.h
+++ components/wasm-opt-cxx-sys/src/shims.h
@@ -6,9 +6,16 @@
 #include <memory> // unique_ptr
 
 namespace wasm {
+  std::unique_ptr<Module> newModule() {
+    return std::make_unique<Module>();
+  }
 
   std::unique_ptr<ModuleReader> newModuleReader() {
     return std::make_unique<ModuleReader>();
+  }
+
+  std::unique_ptr<ModuleWriter> newModuleWriter() {
+    return std::make_unique<ModuleWriter>();
   }
 }
 
