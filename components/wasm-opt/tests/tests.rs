5f7c28b6a4a89da7f6713b7757bbf695e59da586
aimeedeer <aimeez@pm.me>
2022-07-27 18:13:14 -0600

    wip module_reader_text_works

--- components/wasm-opt/tests/tests.rs
+++ components/wasm-opt/tests/tests.rs
@@ -9,6 +9,24 @@ static WAT_FILE: &[u8] = include_bytes!("hello_world.wat");
 static WASM_FILE: &[u8] = include_bytes!("hello_world.wasm");
 
 #[test]
+fn module_reader_text_works() -> anyhow::Result<()> {
+    let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
+    let path = temp_dir.path().join("hello_world.wat");
+
+    let temp_file = File::create(&path)?;
+    let mut writer = BufWriter::new(&temp_file);
+    writer.write_all(WAT_FILE)?;
+
+    let mut m = Module::new();
+    let mut reader = ModuleReader::new();
+    reader.read_text(&path, &mut m)?;
+
+    // todo
+
+    Ok(())
+}
+
+#[test]
 fn module_reader_binary_works() -> anyhow::Result<()> {
     let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
     let path = temp_dir.path().join("hello_world.wasm");
