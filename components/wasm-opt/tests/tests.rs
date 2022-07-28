b7de14a8444ed2d5ec680aa0e763ccc09949ea73
aimeedeer <aimeez@pm.me>
2022-07-27 18:33:03 -0600

    read_write_binary_works

--- components/wasm-opt/tests/tests.rs
+++ components/wasm-opt/tests/tests.rs
@@ -1,6 +1,6 @@
 use wasm_opt::*;
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::BufWriter;
 use std::io::Write;
 use tempfile::Builder;
@@ -9,13 +9,13 @@ static WAT_FILE: &[u8] = include_bytes!("hello_world.wat");
 static WASM_FILE: &[u8] = include_bytes!("hello_world.wasm");
 
 #[test]
-fn module_reader_text_works() -> anyhow::Result<()> {
+fn read_write_text_works() -> anyhow::Result<()> {
     let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
     let path = temp_dir.path().join("hello_world.wat");
 
     let temp_file = File::create(&path)?;
-    let mut writer = BufWriter::new(&temp_file);
-    writer.write_all(WAT_FILE)?;
+    let mut buf_writer = BufWriter::new(&temp_file);
+    buf_writer.write_all(WAT_FILE)?;
 
     let mut m = Module::new();
     let mut reader = ModuleReader::new();
@@ -27,19 +27,32 @@ fn module_reader_text_works() -> anyhow::Result<()> {
 }
 
 #[test]
-fn module_reader_binary_works() -> anyhow::Result<()> {
+fn read_write_binary_works() -> anyhow::Result<()> {
     let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
     let path = temp_dir.path().join("hello_world.wasm");
 
     let temp_file = File::create(&path)?;
-    let mut writer = BufWriter::new(&temp_file);
-    writer.write_all(WASM_FILE)?;
+    let mut buf_writer = BufWriter::new(&temp_file);
+    buf_writer.write_all(WASM_FILE)?;
 
     let mut m = Module::new();
     let mut reader = ModuleReader::new();
     reader.read_binary(&path, &mut m, None)?;
 
-    // todo
+    let mut writer = ModuleWriter::new();
+    let new_file = temp_dir.path().join("hellow_world_by_module_writer.wasm");
+    writer.write_binary(&mut m, &new_file)?;
 
+    let mut another_m = Module::new();
+    let mut another_reader = ModuleReader::new();
+    another_reader.read_binary(&new_file, &mut another_m, None)?;
+    let mut another_writer = ModuleWriter::new();
+    let another_new_file = temp_dir.path().join("hello_world_by_another_module_writer.wasm");
+    another_writer.write_binary(&mut another_m, &another_new_file)?;
+
+    let new_file_reader = fs::read(&new_file)?;
+    let another_new_file_reader = fs::read(&another_new_file)?;
+    
+    assert_eq!(new_file_reader, another_new_file_reader);
     Ok(())
 }
