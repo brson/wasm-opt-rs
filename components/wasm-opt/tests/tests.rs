ec365d4db7202e5bf316d3d574f071cefda94a2e
aimeedeer <aimeez@pm.me>
2022-07-31 16:50:21 -0600

    test pass_options_works

--- components/wasm-opt/tests/tests.rs
+++ components/wasm-opt/tests/tests.rs
@@ -117,3 +117,51 @@ fn pass_runner_works() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn pass_options_works() -> anyhow::Result<()> {
+    let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
+    let path = temp_dir.path().join("hello_world.wasm");
+
+    let temp_file = File::create(&path)?;
+    let mut buf_writer = BufWriter::new(&temp_file);
+    buf_writer.write_all(WASM_FILE)?;
+
+    // Module without optimization
+    let mut m = Module::new();
+    let mut reader = ModuleReader::new();
+    reader.read_binary(&path, &mut m, None)?;
+
+    let mut writer = ModuleWriter::new();
+    let new_file = temp_dir.path().join("hello_world_by_module_writer.wasm");
+    writer.write_binary(&mut m, &new_file)?;
+
+    // Module with optimization
+    let mut another_m = Module::new();
+    let mut another_reader = ModuleReader::new();
+    another_reader.read_binary(&new_file, &mut another_m, None)?;
+
+    let mut pass_options = PassOptions::new();
+    pass_options.set_optimize_level(5);
+    pass_options.set_shrink_level(2_000_000_000);
+
+    let mut pass_runner = PassRunner::new_with_options(&mut another_m, pass_options);
+    pass_runner.add_default_optimization_passes();
+    pass_runner.run();
+    drop(pass_runner);
+
+    let mut another_writer = ModuleWriter::new();
+    let another_new_file = temp_dir
+        .path()
+        .join("hello_world_by_another_module_writer.wasm");
+    another_writer.write_binary(&mut another_m, &another_new_file)?;
+
+    let new_file_reader = fs::read(&new_file)?;
+    let another_new_file_reader = fs::read(&another_new_file)?;
+
+    println!("size 1: {}", new_file_reader.len());
+    println!("size 2: {}", another_new_file_reader.len());
+    assert!(new_file_reader.len() > another_new_file_reader.len());
+
+    Ok(())
+}
