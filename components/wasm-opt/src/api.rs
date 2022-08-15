85722019244b3fb99943b1cfcede73666b5f4d7a
aimeedeer <aimeez@pm.me>
2022-08-14 18:38:55 -0600

    move OptimizationOptions constructors to api.rs

--- components/wasm-opt/src/api.rs
+++ components/wasm-opt/src/api.rs
@@ -77,6 +77,78 @@ impl Default for FileType {
     }
 }
 
+impl OptimizationOptions {
+    pub fn new_s() -> Self {
+        OptimizationOptions::default()
+    }
+
+    pub fn new_0() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level0;
+        passopts.shrink_level = ShrinkLevel::Level0;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+
+    pub fn new_1() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level1;
+        passopts.shrink_level = ShrinkLevel::Level0;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+
+    pub fn new_2() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level2;
+        passopts.shrink_level = ShrinkLevel::Level0;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+
+    pub fn new_3() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level3;
+        passopts.shrink_level = ShrinkLevel::Level0;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+
+    pub fn new_4() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level4;
+        passopts.shrink_level = ShrinkLevel::Level0;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+
+    pub fn new_z() -> Self {
+        let mut passopts = PassOptions::default();
+        passopts.optimize_level = OptimizeLevel::Level2;
+        passopts.shrink_level = ShrinkLevel::Level2;
+
+        let mut opts = OptimizationOptions::default();
+        opts.passopts = passopts;
+
+        opts
+    }
+}
+
 impl Default for InliningOptions {
     fn default() -> InliningOptions {
         InliningOptions {
