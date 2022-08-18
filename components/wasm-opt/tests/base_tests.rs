c11479e7c5a17928d51b7101336154e585930e87
aimeedeer <aimeez@pm.me>
2022-08-18 15:29:36 -0600

    test unicode

--- components/wasm-opt/tests/base_tests.rs
+++ components/wasm-opt/tests/base_tests.rs
@@ -11,6 +11,41 @@ static GARBAGE_FILE: &[u8] = include_bytes!("garbage_file.wat");
 static MULTISIG_WASM: &[u8] = include_bytes!("ink_example_multisig.wasm");
 
 #[test]
+fn read_write_from_unicode_works() -> anyhow::Result<()> {
+    let temp_dir = Builder::new().prefix("α℗$∞ℳ").tempdir()?;
+    let path = temp_dir.path().join("hello_world.wasm");
+
+    let temp_file = File::create(&path)?;
+    let mut buf_writer = BufWriter::new(&temp_file);
+    buf_writer.write_all(WASM_FILE)?;
+
+    let mut m = Module::new();
+    let mut reader = ModuleReader::new();
+    reader.read_binary(&path, &mut m, None)?;
+
+    let mut writer = ModuleWriter::new();
+    let new_file = temp_dir.path().join("hello_world_by_module_writer.wasm");
+    writer.write_binary(&mut m, &new_file)?;
+
+    let mut another_m = Module::new();
+    let mut another_reader = ModuleReader::new();
+    another_reader.read_binary(&new_file, &mut another_m, None)?;
+
+    let mut another_writer = ModuleWriter::new();
+    let another_new_file = temp_dir
+        .path()
+        .join("hello_world_by_another_module_writer.wasm");
+    another_writer.write_binary(&mut another_m, &another_new_file)?;
+
+    let new_file_reader = fs::read(&new_file)?;
+    let another_new_file_reader = fs::read(&another_new_file)?;
+
+    assert_eq!(new_file_reader, another_new_file_reader);
+
+    Ok(())
+}
+
+#[test]
 fn read_write_text_works() -> anyhow::Result<()> {
     let temp_dir = Builder::new().prefix("wasm_opt_tests").tempdir()?;
     let path = temp_dir.path().join("hello_world.wat");
